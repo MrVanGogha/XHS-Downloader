b15af2fd0c83c6ef8df0c4930f60272b0031ff88
zhaohh <Mr_VanGogh@yeah.net>
2025-11-18 15:48:28 +0800

    feat(video): 添加获取视频直链地址功能
    在Video类中新增get_video_direct_link方法用于提取视频直链地址，并在XHS类的__extract_video方法中调用以丰富返回数据

--- source/application/video.py
+++ source/application/video.py
@@ -18,6 +18,23 @@ class Video:
             if (t := data.safe_extract(".".join(cls.VIDEO_LINK)))
             else []
         )
+    
+    VIDEO_DIRECT_LINK = (
+        "video",
+        "media",
+        "stream",
+        "h264[0]",
+        "masterUrl",
+    )
+    @classmethod
+    def get_video_direct_link(cls, data: Namespace) -> list:
+        return (
+            [Html.format_url(f"{t}")]
+            if (t := data.safe_extract(".".join(cls.VIDEO_DIRECT_LINK)))
+            else []
+        )
+
+
 
     VIDEO_DURATION = (
         "video",
